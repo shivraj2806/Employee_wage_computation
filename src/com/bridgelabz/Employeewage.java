e16b85dcb7b7d6e86ea0e4e6e4fa141bef8e9fe2
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-09 22:50:16 +0530

    Added:Employee_wage_using_switch_case

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -1,15 +1,28 @@
 package com.bridgelabz;
 
 public class Employeewage {
-        public static void main(String[] args){
-            int is_present = 1;
-            double wage_per_Hr = 200;
-            double full_day_Hr = 8;
-            double daily_emp_wage = wage_per_Hr * full_day_Hr;
-            double employeecheck = Math.floor(Math.random()*10) % 2;
-            if (employeecheck == is_present)
-                System.out.println("employee is present and daily salary is"+daily_emp_wage);
-            else
-                System.out.println("employee is absent");
+        public static final int is_part_time = 1;
+        public static final int is_full_time = 2;
+        public static final int employee_wage_per_Hr = 20;
+
+        public static void main(String[] args) {
+            System.out.println("calculate employee wage");
+
+            int empHr = 0;
+            int empWage = 0;
+            int employeecheck = (int)Math.floor(Math.random() * 10) % 3;
+            switch (employeecheck) {
+                case is_full_time:
+                    empHr = 4;
+                    break;
+                case is_part_time:
+                    empHr = 8;
+                    break;
+                default:
+                    empHr = 0;
+
+            }
+            empWage = empHr * employee_wage_per_Hr;
+            System.out.println("employee wage is" + empWage);
         }
     }
