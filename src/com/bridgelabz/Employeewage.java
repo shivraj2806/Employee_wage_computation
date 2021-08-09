0dc01c46e93e5fedf7babc308a777761384d61f4
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-09 23:19:45 +0530

    Added:total_employee_wage

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -1,16 +1,20 @@
 package com.bridgelabz;
 
 public class Employeewage {
-        public static final int is_part_time = 1;
-        public static final int is_full_time = 2;
-        public static final int employee_wage_per_Hr = 20;
+    public static final int is_part_time = 1;
+    public static final int is_full_time = 2;
+    public static final int employee_wage_per_Hr = 20;
+    public static final int NO_of_working_days = 20;
 
-        public static void main(String[] args) {
-            System.out.println("calculate employee wage");
+    public static void main(String[] args) {
+        System.out.println("calculate employee wage");
 
-            int empHr = 0;
-            int empWage = 0;
-            int employeecheck = (int)Math.floor(Math.random() * 10) % 3;
+        int empHr = 0;
+        int empWage = 0;
+        int total_emp_wage = 0;
+
+        int employeecheck = (int) Math.floor(Math.random() * 10) % 3;
+        for (int day = 0; day <= NO_of_working_days; day++) {
             switch (employeecheck) {
                 case is_full_time:
                     empHr = 4;
@@ -23,6 +27,9 @@ public class Employeewage {
 
             }
             empWage = empHr * employee_wage_per_Hr;
+            total_emp_wage += empWage;
             System.out.println("employee wage is" + empWage);
         }
+        System.out.println(+total_emp_wage);
     }
+}
