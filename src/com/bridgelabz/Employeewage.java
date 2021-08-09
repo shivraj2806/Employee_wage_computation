a0f6ab41d87034acc94f5ea96001b5904bfb8f11
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-09 21:59:25 +0530

    Added: employee attendence

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -2,7 +2,13 @@ package com.bridgelabz;
 
 public class Employeewage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+        System.out.println("welcome to employee wage computation");
+    int is_present = 1;
+    double employeecheck = Math.floor(Math.random()*10) % 2;
+    if (employeecheck == is_present)
+            System.out.println("employee is present");
+    else
+            System.out.println("employee is absent");
 
     }
 }
