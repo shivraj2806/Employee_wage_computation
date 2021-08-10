daa335d33ef75240173ee8117acfb0dbaf96d2cf
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-10 15:30:02 +0530

    Added:wage

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -5,16 +5,18 @@ public class Employeewage {
     public static final int is_full_time = 2;
     public static final int employee_wage_per_Hr = 20;
     public static final int NO_of_working_days = 20;
-
+    public static final int max_hr_in_month = 100;
     public static void main(String[] args) {
         System.out.println("calculate employee wage");
 
         int empHr = 0;
         int empWage = 0;
-        int total_emp_wage = 0;
+        int total_emp_hr = 0;
+        int total_working_days=0;
 
         int employeecheck = (int) Math.floor(Math.random() * 10) % 3;
-        for (int day = 0; day <= NO_of_working_days; day++) {
+        while (total_emp_hr<=max_hr_in_month&&total_working_days<NO_of_working_days){
+            total_working_days++;
             switch (employeecheck) {
                 case is_full_time:
                     empHr = 4;
@@ -26,10 +28,10 @@ public class Employeewage {
                     empHr = 0;
 
             }
-            empWage = empHr * employee_wage_per_Hr;
-            total_emp_wage += empWage;
-            System.out.println("employee wage is" + empWage);
+            total_emp_hr +=empHr;
+            System.out.println("Day#:" + total_working_days + "emp hr:"+empHr);
         }
-        System.out.println(+total_emp_wage);
+       int total_emp_wage = total_emp_hr * employee_wage_per_Hr;
+        System.out.println("total emp wage:"+total_emp_wage);
     }
 }
